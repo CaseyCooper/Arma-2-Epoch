dc99670ca90e496cfd165ba93503ca2187251616
Casey Cooper <caseycooper843@gmail.com>
2017-08-02 16:38:07 -0400

    changing cooked meat prices

--- MPMissions/DayZ_Epoch_24.Napf/custom/CfgServerTrader/Category/FriendlyCookedMeats.hpp
+++ MPMissions/DayZ_Epoch_24.Napf/custom/CfgServerTrader/Category/FriendlyCookedMeats.hpp
@@ -1,48 +1,48 @@
 class Category_630 {
 	class FoodbaconCooked {
 		type = "trade_items";
-		buy[] = {4,"ItemSilverBar"};
-		sell[] = {2,"ItemSilverBar"};
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
 	};
 	class FoodbeefCooked {
 		type = "trade_items";
-		buy[] = {4,"ItemSilverBar"};
-		sell[] = {2,"ItemSilverBar"};
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
 	};
 	class FoodchickenCooked {
 		type = "trade_items";
-		buy[] = {4,"ItemSilverBar"};
-		sell[] = {2,"ItemSilverBar"};
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
 	};
 	class FoodGoatCooked {
 		type = "trade_items";
-		buy[] = {4,"ItemSilverBar"};
-		sell[] = {2,"ItemSilverBar"};
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
 	};
 	class FoodmuttonCooked {
 		type = "trade_items";
-		buy[] = {4,"ItemSilverBar"};
-		sell[] = {2,"ItemSilverBar"};
+		buy[] = {2,"ItemGoldBar"};
+		sell[] = {1,"ItemGoldBar"};
 	};
 	class FoodrabbitCooked {
 		type = "trade_items";
-		buy[] = {2,"ItemSilverBar10oz"};
-		sell[] = {1,"ItemSilverBar10oz"};
+		buy[] = {5,"ItemGoldBar"};
+		sell[] = {5,"ItemGoldBar"};
 	};
 	class FishCookedTrout { // most common
 		type = "trade_items";
 		buy[] = {2,"ItemGoldBar"};
-		sell[] = {1,"ItemSilverBar10oz"};
+		sell[] = {1,"ItemGoldBar"};
 	};
 	class FishCookedSeaBass { // less common
 		type = "trade_items";
-		buy[] = {3,"ItemGoldBar"};
-		sell[] = {2,"ItemSilverBar10oz"};
+		buy[] = {2,"ItemGoldBar10oz"};
+		sell[] = {1,"ItemGoldBar10oz"};
 	};
 	class FishCookedTuna { // rarest
 		type = "trade_items";
-		buy[] = {4,"ItemGoldBar"};
-		sell[] = {1,"ItemGoldBar"};
+		buy[] = {1,"ItemBriefcase100oz"};
+		sell[] = {1,"ItemBriefcase100oz"};
 	};
 };
 
